:construction: fixed type hints, added quaternion conversion

src/kinematics/kinematics_solver.py
@@ -4,6 +4,7 @@ import rospy
 import tf2_ros as tf2
 from sensor_msgs.msg import JointState
 from pose_service import JointData
+from geometry_msgs.msg import Quaternion
 
 # link lengths of the robot in meters
 L12 = 18e-3
@@ -27,7 +28,8 @@ class KinematicsSolver:
     def run(self):
         while not rospy.is_shutdown():
             if len(self.cur_joint_state.position) > 0:
-                pos, R = self.forwards_kinematics()
+                joint_data = JointData.from_joint_state(self.cur_joint_state)
+                pos, R = self.forwards_kinematics(joint_data)
                 rospy.loginfo(f"End effector position: {pos}")
                 rospy.loginfo(f"End effector rotation matrix: {R}")
             self.rate.sleep()
@@ -37,29 +39,28 @@ class KinematicsSolver:
             rospy.loginfo("Received new joint state")
         self.cur_joint_state = msg
 
-    def get_DH_matrix(self, joint_state: JointState):
+    def get_DH_matrix(self, joint_data: JointData):
         """ "
         columns are alpha, d, a, theta
         """
         PI_2 = np.pi / 2
-        cur_joint_data = JointData.from_joint_state(joint_state)
         matrix = np.array(
             [
-                [-PI_2, L12, 0.0, cur_joint_data.joint1],
-                [0.0, 0.0, L23, cur_joint_data.joint2 - PI_2],
-                [0.0, 0.0, L34, cur_joint_data.joint3],
-                [0.0, 0.0, L45, cur_joint_data.joint4],
-                [cur_joint_data.joint5, 0.0, LEND, 0.0],
+                [-PI_2, L12, 0.0, joint_data.joint1],
+                [0.0, 0.0, L23, joint_data.joint2 - PI_2],
+                [0.0, 0.0, L34, joint_data.joint3],
+                [0.0, 0.0, L45, joint_data.joint4],
+                [joint_data.joint5, 0.0, LEND, 0.0],
             ]
         )
 
         return matrix
 
-    def get_DH_transform(self, joint_state: JointState, link_number: int):
+    def get_DH_transform(self, joint_data: JointData, link_number: int):
         """
         returns the transform matrix for the given link number
         """
-        alpha, d, a, theta = self.get_DH_matrix(joint_state)[link_number, :]
+        alpha, d, a, theta = self.get_DH_matrix(joint_data)[link_number, :]
         ca = np.cos(alpha)
         sa = np.sin(alpha)
         ct = np.cos(theta)
@@ -74,75 +75,75 @@ class KinematicsSolver:
             ]
         )
 
-    def get_transform(self, joint_state: JointState, link_number: int):
+    def get_transform(self, joint_data: JointData, link_number: int):
         """
         returns the transform matrix from the base to the given link number
         """
         transform = np.eye(4)
         for i in range(link_number):
-            transform = np.dot(transform, self.get_DH_transform(joint_state, i))
+            transform = np.dot(transform, self.get_DH_transform(joint_data, i))
 
         return transform
 
-    def forwards_kinematics(self, joint_state: JointState):
+    def forwards_kinematics(self, joint_data: JointData):
         """
         returns the transform matrix from the base to the end effector
         """
-        transform = self.get_transform(joint_state, 5)
+        transform = self.get_transform(joint_data, 5)
         R = transform[:3, :3]
         pe0 = np.array([0.0, 0.0, 0.0, 1.0]).transpose()
         pe_b = transform.dot(pe0)
         return pe_b[:3], R
 
-    def get_end_effector_pose(self, joint_state: JointState):
+    def get_end_effector_pose(self, joint_data: JointData):
         """
         returns the transform matrix from the base to the end effector
         """
-        transform = self.get_transform(joint_state, 5)
+        transform = self.get_transform(joint_data, 5)
         pe0 = np.array([0.0, 0.0, 0.0, 1.0]).transpose()
         pe_b = transform.dot(pe0)
         return pe_b[:3]
 
-    def get_link_pose(self, joint_state: JointState, link_number: int):
+    def get_link_pose(self, joint_data: JointData, link_number: int):
         """
         returns the transform matrix from the base to the given link number
         """
-        transform = self.get_transform(joint_state, link_number)
+        transform = self.get_transform(joint_data, link_number)
         pe0 = np.array([0.0, 0.0, 0.0, 1.0]).transpose()
         pe_b = transform.dot(pe0)
 
         return pe_b[:3]
 
-    def get_link_normal(self, joint_state: JointState, link_number: int):
+    def get_link_normal(self, joint_data: JointData, link_number: int):
         """
         returns the normal vector of the given link number
         """
-        transform = self.get_transform(joint_state, link_number)
+        transform = self.get_transform(joint_data, link_number)
         R = transform[:3, :3]
         zi0 = np.array([0.0, 0.0, 1.0]).transpose()
         zi_b = R.dot(zi0)
 
         return zi_b
 
-    def get_jacobian_column(self, joint_state: JointState, link_number: int):
+    def get_jacobian_column(self, joint_data: JointData, link_number: int):
         """
         returns the jacobian column for the given link number
         """
-        zi = self.get_link_normal(joint_state, link_number)
-        pi = self.get_link_pose(joint_state, link_number)
+        zi = self.get_link_normal(joint_data, link_number)
+        pi = self.get_link_pose(joint_data, link_number)
         pe = self.get_end_effector_pose(
-            joint_state,
+            joint_data,
         )
 
         return np.concatenate([np.cross(zi, pe - pi), zi])
 
-    def get_jacobian(self, joint_state: JointState):
+    def get_jacobian(self, joint_data: JointData):
         """
         returns the jacobian matrix
         """
         jacobian = np.zeros((6, 5))
         for i in range(5):
-            jacobian[:, i] = self.get_jacobian_column(joint_state, i)
+            jacobian[:, i] = self.get_jacobian_column(joint_data, i)
 
         return jacobian
 
@@ -152,7 +153,7 @@ class KinematicsSolver:
         """
         return np.linalg.pinv(jacobian)
 
-    def loss(self, error):
+    def loss(self, error: np.ndarray):
 
         return error.dot(error)
 
@@ -171,19 +172,18 @@ class KinematicsSolver:
         self,
         desired_pos,
         desired_R,
-        joint_state,
+        joint_state: JointData,
         tol=1e-4,
         lr=1,
         verbose=False,
-        debug=False,
     ):
         """
         fast convergence on position, but slow on R.
         """
-        R = np.array(R)
+        R = np.array(desired_R)
         X_hat, R_hat = self.forwards_kinematics()
-        X = point
-        theta = np.array(joint_positions)
+        X = desired_pos
+        theta = joint_state.to_np_array()
         k = 1
         _loss = self.loss(self.get_error_vector(X, X_hat, R, R_hat))
         if verbose:
@@ -194,7 +194,7 @@ class KinematicsSolver:
             error = self.get_error_vector(X, X_hat, R, R_hat)
             err_theta = inverse_jacobian.dot(error)
             theta = theta - lr * err_theta
-            X_hat, R_hat = self.forwards_kinematics(theta)
+            X_hat, R_hat = self.forwards_kinematics(JointData.from_np_array(theta))
             _loss = self.loss(error)
             k += 1
             lr /= 10
@@ -203,6 +203,27 @@ class KinematicsSolver:
                     print("timeout")
                 raise RuntimeError()
         return theta
+
+    def ros_quaternion_to_rotation(self, quaternion: Quaternion):
+        return np.array(
+            [
+                [
+                    1 - 2 * (quaternion.y**2 + quaternion.z**2),
+                    2 * (quaternion.x * quaternion.y - quaternion.z * quaternion.w),
+                    2 * (quaternion.x * quaternion.z + quaternion.y * quaternion.w),
+                ],
+                [
+                    2 * (quaternion.x * quaternion.y + quaternion.z * quaternion.w),
+                    1 - 2 * (quaternion.x**2 + quaternion.z**2),
+                    2 * (quaternion.y * quaternion.z - quaternion.x * quaternion.w),
+                ],
+                [
+                    2 * (quaternion.x * quaternion.z - quaternion.y * quaternion.w),
+                    2 * (quaternion.y * quaternion.z + quaternion.x * quaternion.w),
+                    1 - 2 * (quaternion.x**2 + quaternion.y**2),
+                ],
+            ]
+        )
 
 
 if __name__ == "__main__":