b91a8516c1d586d8bb8a1d083cb51a98583d3f94
DopeyRobot <dopeyrobot5000@gmail.com>
2023-02-17 15:35:17 +0100

    :tada: IK service

--- src/kinematics/src/IK_service.py
+++ src/kinematics/src/IK_service.py
@@ -3,57 +3,43 @@ import rospy
 from kinematics_utils import JointData, RefPoses
 from kinematics_solver import KinematicsSolver, D1, A2, A3, D5
 from sensor_msgs.msg import JointState
-from kinematics.srv import JointAngles
+from kinematics.srv import JointAngles, IKData
+from std_srvs.srv import Empty, EmptyResponse, EmptyRequest
 import numpy as np
 
 class IKService:
-
+    """
+    Service node to move the robot to a specific position in space
+    """
     def __init__(self) -> None:
         self.solver = KinematicsSolver()
         self.joint_state_sub = rospy.Subscriber("/joint_states", JointState, self.joint_state_callback)
         self.cur_joint_state = JointState()
         rospy.wait_for_service("pose_service", 5.0)
         self.pose_service = rospy.ServiceProxy("pose_service", JointAngles)
+        self.inverse_k_service = rospy.Service("IK_service", IKData, self.IK_callback)
 
         while len(self.cur_joint_state.position) < 5:
             rospy.sleep(0.1)
 
-        sol = IKService.analytical_IK([-150e-3,0e-3,-50e-3],np.pi)
-        self.pose_service.call(sol.to_np_array(), 2000)
+    def IK_callback(self, req:IKData):
+        pos = req.x, req.y, req.z
+        psi = req.wrist_pitch
+        rot = req.gripper_rotation
+
+        self.move_to(pos, psi, rot)
+
+        return EmptyResponse()
 
     def joint_state_callback(self, msg):
         self.cur_joint_state = msg
 
-    def analytical_IK(desired_pos, desired_wrist_pitch, desired_theta5 = 0.0):
-        """
-        Solves the analytical IK sometimes
-        """
-        
-        try:
-            psi = desired_wrist_pitch
-            xd, yd, zd = desired_pos
-            theta1 = np.arctan2(yd, xd)%(2*np.pi) - np.pi
-            rd = np.hypot(xd, yd)
-            z4 = zd - D5*np.cos(psi)
-            r4 = rd - D5*np.sin(psi)
-
-            alpha = np.arctan2(r4, z4-D1)
-            S = np.hypot(z4-D1, r4)
-            cb = (S**2 + A2**2 - A3**2)/(2*S*A2)
-            c3 = (S*cb-A2)/(A3)
-
-            beta= np.arccos(cb)
-            theta2 = alpha - beta
-            theta3 = np.arccos(c3)
-            theta4 = (psi - theta2 - theta3)%(2*np.pi)
-            theta5 = desired_theta5
-
-            return JointData.from_np_array(
-                -np.array([-theta1, theta2, theta3, theta4, theta5])
-                )
-        except:
-            rospy.logerr("Unreachable pose")
-            return RefPoses.HOME.value
+    def move_to(self, pos, wrist_pitch, wrist_rotation):
+        sol = self.solver.analytical_IK(pos, wrist_pitch, wrist_rotation)
+        if sol is not None:
+            self.pose_service(sol.to_np_array(), 2000)
+        else:
+            return
 
 if __name__ == "__main__":
     rospy.init_node("IK_ser")
