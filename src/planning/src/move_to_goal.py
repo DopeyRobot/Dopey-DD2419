b147ee82ffda30292ab16ea2ee73ef2cce8d7744
majaastrand <jastran@kth.se>
2023-02-22 18:46:17 +0100

    Add new goal angle

--- src/planning/src/move_to_goal.py
+++ src/planning/src/move_to_goal.py
@@ -17,11 +17,11 @@ class move_to_goal():
 
         self.Kp_ang = -0.05
         self.Ki_ang = -0.0003
-        self.Ki_ang = 0.3
         self.Kd_ang = 0.03
         self.Kp_dist = 0.06
         self.Ki_dist = 0.0002
         self.Kp_ang2 = 0.03
+        self.Ki_ang2 = 0.0
         self_Kd_dist = 0.0
 
         self.integral_error_ang = 0.0
@@ -36,12 +36,9 @@ class move_to_goal():
  
         self.goal_theta = 0.0
         self.odom_theta = 0.0
+        self.goal_pose = None
+        self.transformed_goal_pose = None #PoseStamped()
 
-        self.transformed_goal_pose.pose.position.x = 100
-        self.transformed_goal_pose.pose.position.y = 100
-
-        self.goal_pose = PoseStamped()
-        self.transformed_goal_pose = PoseStamped()
         self.twist = Twist()  
         self.odom = Odometry()
 
@@ -53,8 +50,9 @@ class move_to_goal():
         self.rate = rospy.Rate(self.f)
         self.targetframe = "base_link"
         self.currentframe = "odom"
-        self.timeout = rospy.Duration(1)
-    
+        self.timeout = rospy.Duration(2)
+
+        rospy.sleep(2)
 
         self.publisher_twist = rospy.Publisher('motor_controller/twist', Twist, queue_size=10)
         self.goal_subscriber = rospy.Subscriber('move_base_simple/goal', PoseStamped, self.goal_callback) 
@@ -65,11 +63,13 @@ class move_to_goal():
 
 
     def goal_callback(self, msg):
+        rospy.loginfo("new goal ")
+        self.goal_pose = PoseStamped()
         self.goal_pose.pose = msg.pose
         self.goal_pose.header.stamp = msg.header.stamp
         self.goal_pose.header.frame_id = msg.header.frame_id
+        self.arrived2point = False
         
-
 
     def odom_callback(self, msg):
         self.odom = msg
@@ -80,61 +80,77 @@ class move_to_goal():
     def run(self):
 
         while not rospy.is_shutdown():
+            if self.goal_pose:
+                self.goal_pose.header.frame_id = self.currentframe
+                self.goal_pose.header.stamp = rospy.Time.now()
+                self.transformed_goal_pose = self.tf_buffer.transform(self.goal_pose, self.targetframe, self.timeout)
+                rot_q = self.transformed_goal_pose.pose.orientation
+                (_, _, self.goal_theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
+                self.error_dist = math.sqrt(self.transformed_goal_pose.pose.position.x**2 + self.transformed_goal_pose.pose.position.y**2)
+                self.error_ang = math.atan2(self.transformed_goal_pose.pose.position.y, self.transformed_goal_pose.pose.position.x)
 
-            self.transformed_goal_pose = self.tf_buffer.transform(self.goal_pose, self.targetframe, self.timeout)
+                self.goal_theta -= (self.goal_theta + math.pi % 2*math.pi) -math.pi
 
-            rot_q = self.transformed_goal_pose.pose.orientation
-            (_, _, self.goal_theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-            self.goal_theta -= math.pi
+                error_dist = math.sqrt(self.transformed_goal_pose.pose.position.x**2 + self.transformed_goal_pose.pose.position.y**2)
+                error_ang = math.atan2(self.transformed_goal_pose.pose.position.y, self.transformed_goal_pose.pose.position.x)
+                error_ang2 = self.odom_theta - self.goal_theta
 
-            error_dist = math.sqrt(self.transformed_goal_pose.pose.position.x**2 + self.transformed_goal_pose.pose.position.y**2)
-            error_ang = math.atan2(self.transformed_goal_pose.pose.position.y, self.transformed_goal_pose.pose.position.x)
-            error_ang2 = self.odom_theta - self.goal_theta
+                proportional_output_ang = self.Kp_ang * error_ang
+                self.integral_error_ang += error_ang
+                integral_output_ang = self.Ki_ang * self.integral_error_ang
+                total_output_ang = proportional_output_ang + integral_output_ang
 
-            proportional_output_ang = self.Kp_ang * error_ang
-            self.integral_error_ang += error_ang
-            integral_output_ang = self.Ki_ang * self.integral_error_ang
-            total_output_ang = proportional_output_ang + integral_output_ang
+                proportional_output_dist = self.Kp_dist * error_dist
+                self.integral_error_dist += error_dist
+                integral_output_dist = self.Ki_dist * self.integral_error_dist
+                total_output_dist = proportional_output_dist #+ integral_output_dist
 
-            proportional_output_dist = self.Kp_dist * error_dist
-            self.integral_error_dist += error_dist
-            integral_output_dist = self.Ki_dist * self.integral_error_dist
-            total_output_dist = proportional_output_dist #+ integral_output_dist
+                proportional_output_ang2 = self.Kp_ang2 * error_ang2
+                self.integral_error_ang2 += error_ang2
+                integral_output_ang2 = self.Ki_ang2 * self.integral_error_ang2
+                total_output_ang2 = proportional_output_ang2
 
-            proportional_output_ang2 = self.Kp_ang2 * error_ang2
-            self.integral_error_ang2 += error_ang2
-            integral_output_ang2 = self.Ki_ang2 * self.integral_error_ang2
-            total_output_ang2 = proportional_output_ang2
+                total_output_dist = proportional_output_dist #+ integral_output_dist
 
-            
+                #print('error dist', self.error_dist)
+                #print(self.goal_theta)
+                #print('error: ', abs(error_ang2))
 
-            if not self.arrived2point:
-                if abs(self.error_ang) > self.threshold_ang:
-                    self.twist.angular.z = total_output_ang 
-                    self.twist.linear.x = 0
-    
-                elif self.error_dist > self.threshold_dist:
-                    self.twist.linear.x = total_output_dist
-                    self.twist.angular.z = 0.0
+                
 
+                if not self.arrived2point:
+                    if abs(self.error_ang) > self.threshold_ang:
+                        # rospy.loginfo("HERE1")
+                        self.twist.angular.z = total_output_ang 
+                        self.twist.linear.x = 0
+        
+                    elif self.error_dist > self.threshold_dist:
+                        # rospy.loginfo("HERE2")
+                        self.twist.linear.x = total_output_dist
+                        self.twist.angular.z = 0.0
+
+                    else:
+                        # rospy.loginfo("HERE3")
+                        self.twist.angular.z = 0
+                        self.twist.linear.x = 0
+                        total_output_ang = 0
+                        total_output_dist = 0
+                        self.arrived2point = True
                 else:
-                    self.twist.angular.z = 0
-                    self.twist.linear.x = 0
-                    total_output_ang = 0
-                    total_output_dist = 0
-                    self.arrived2point = True
-            else:
-                if abs(error_ang2) > self.threshold_ang:
-                    self.twist.linear.x = 0.0
-                    self.twist.angular.z = total_output_ang2
-                    self.rot_clear2 = False
+                    if abs(error_ang2) > self.threshold_ang:
+                        # rospy.loginfo("HERE4")
+                        print(self.goal_theta-self.odom_theta)
+                        self.twist.linear.x = 0.0
+                        self.twist.angular.z = total_output_ang2
+                        self.rot_clear2 = False
 
-                else:
-                    self.twist.linear.x = 0.0
-                    self.twist.angular.z = 0.0
+                    else:
+                        # rospy.loginfo("HERE5")
+                        self.twist.linear.x = 0.0
+                        self.twist.angular.z = 0.0
 
 
-            self.publisher_twist.publish(self.twist)
+                self.publisher_twist.publish(self.twist)
             self.rate.sleep()
 
 
