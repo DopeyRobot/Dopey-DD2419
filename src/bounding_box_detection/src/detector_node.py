b0cc039aceed3764e0de580e9bef0ceefb9f0dd4
DopeyRobot <dopeyrobot5000@gmail.com>
2023-03-02 19:42:31 +0100

    :construction: depth

--- src/bounding_box_detection/src/detector_node.py
+++ src/bounding_box_detection/src/detector_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from typing import List
 import rospy
 import torch
 from detector import Detector
@@ -16,26 +17,35 @@ torch.cuda.set_per_process_memory_fraction(0.8, 0)
 torch.cuda.empty_cache()
 class BoundingBoxNode:
     def __init__(self) -> None:
+        self.f = 60
+        self.rate = rospy.Rate(self.f)
+
         self.camera_topic = "/camera/color/image_raw"
         self.out_image_topic = "/camera/color/image_bbs"
+        self.depth_topic = "/camera/aligned_depth_to_color/image_raw"
         self.model_path = "/home/robot/dd2419_ws/src/bounding_box_detection/src/det_2023-03-02_14-23-34-390133.pt"
+        
         self.image_subscriber = rospy.Subscriber(self.camera_topic, Image, self.image_callback)
+        self.depth_subsriber = rospy.Subscriber(self.depth_topic, Image, self.depth_callback)
         self.image_publisher = rospy.Publisher(self.out_image_topic, Image, queue_size=10)
         self.bridge = CvBridge()
-        self.bb_publisher = None
-        self.f = 60
+        
+        self.ros_img = None
+        self.ros_depth = None
+        self.image = None
+        self.array_image = None
+        self.depth = None
+        self.array_depth = None
+
         self.verbose = False
-        self.rate = rospy.Rate(self.f)
         # self.model = Detector()
-        self.model=torch.load(self.model_path)
+        self.model= torch.load(self.model_path)
         self.model.eval()
         self.cuda = torch.cuda.is_available()
-        self.ros_img = None
         
         if self.cuda:
             self.model.cuda()
 
-        self.image = None
 
         self.run()
 
@@ -44,26 +54,48 @@ class BoundingBoxNode:
         image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
         self.image = PILImage.fromarray(image)
         self.array_image = np.asarray(image)
+    
+    def depth_callback(self, msg):
+        self.ros_depth = msg
+        depth = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        self.depth = PILImage.fromarray(depth)
+        self.array_depth = np.asarray(depth)
+
+    
+    def predict(self, image) -> List[utils.BoundingBox]:
+        torch_image, _ = self.model.input_transform(image, [], validation=True)
+        if self.cuda:
+            torch_image = torch_image.to("cuda")
+        out = self.model(torch_image.unsqueeze(0)).cpu()
+        bbs = self.model.decode_output(out, 0.5, scale_bb=True)[0]
+        return bbs
+    
+    def show_bbs_in_image(self, bbs: List[utils.BoundingBox], image) -> None:
+        if len(bbs) >0:
+            image = utils.draw_bb_on_image(image, bbs, utils.CLASS_DICT)
+            ros_img = self.bridge.cv2_to_imgmsg(image,  encoding="passthrough")
+            self.image_publisher.publish(ros_img)
+        else:
+            self.image_publisher.publish(self.ros_img)
+                
+
     def run(self):
         while not rospy.is_shutdown():
             if self.image is not None:
                 if self.verbose:
                     start = time.time()
-                torch_image, _ = self.model.input_transform(self.image.copy(), [], validation=True)
-                if self.cuda:
-                    torch_image = torch_image.to("cuda")
-                out = self.model(torch_image.unsqueeze(0)).cpu()
-                bbs = self.model.decode_output(out, 0.5, scale_bb=True)[0]
-                if len(bbs) >0:
-                    bbs = utils.non_max_suppresion(bbs, confidence_threshold=0.80, diff_class_thresh=0.75)
-                    image = utils.draw_bb_on_image(self.array_image, bbs, utils.CLASS_DICT)
-                    ros_img = self.bridge.cv2_to_imgmsg(image,  encoding="passthrough")
-                    self.image_publisher.publish(ros_img)
-                else:
-                    self.image_publisher.publish(self.ros_img)
-                
+                bbs = self.predict(self.image.copy())
+                #supress multiple bbs
+                bbs = utils.non_max_suppresion(bbs, confidence_threshold=0.80, diff_class_thresh=0.75)
+                #add bbs to image and publish
+                self.show_bbs_in_image(bbs, self.array_image)
+
                 if self.verbose:
                     rospy.loginfo(f"full inference time = {time.time() - start}")
+            
+            if self.depth is not None:
+                rospy.loginfo(f"depth = {self.array_depth[100,100]}")
+                
             
             self.rate.sleep()
         
