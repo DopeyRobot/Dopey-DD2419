58af42c62802bd33ae6100d82eea868d1b0bf081
DopeyRobot <dopeyrobot5000@gmail.com>
2023-03-02 16:49:58 +0100

    :construction: test new model

--- src/bounding_box_detection/src/detector_node.py
+++ src/bounding_box_detection/src/detector_node.py
@@ -18,7 +18,7 @@ class BoundingBoxNode:
     def __init__(self) -> None:
         self.camera_topic = "/camera/color/image_raw"
         self.out_image_topic = "/camera/color/image_bbs"
-        self.model_path = "/home/robot/dd2419_ws/src/bounding_box_detection/src/det_2023-02-21_19-29-24-367975.pt"
+        self.model_path = "/home/robot/dd2419_ws/src/bounding_box_detection/src/det_2023-03-02_14-23-34-390133.pt"
         self.image_subscriber = rospy.Subscriber(self.camera_topic, Image, self.image_callback)
         self.image_publisher = rospy.Publisher(self.out_image_topic, Image, queue_size=10)
         self.bridge = CvBridge()
@@ -26,8 +26,8 @@ class BoundingBoxNode:
         self.f = 60
         self.verbose = False
         self.rate = rospy.Rate(self.f)
-        self.model = Detector()
-        self.model.load_state_dict(torch.load(self.model_path))
+        # self.model = Detector()
+        self.model=torch.load(self.model_path)
         self.model.eval()
         self.cuda = torch.cuda.is_available()
         self.ros_img = None
@@ -55,7 +55,7 @@ class BoundingBoxNode:
                 out = self.model(torch_image.unsqueeze(0)).cpu()
                 bbs = self.model.decode_output(out, 0.5, scale_bb=True)[0]
                 if len(bbs) >0:
-                    bbs = utils.non_max_suppresion(bbs, confidence_threshold=0.75, diff_class_thresh=0.6)
+                    bbs = utils.non_max_suppresion(bbs, confidence_threshold=0.80, diff_class_thresh=0.75)
                     image = utils.draw_bb_on_image(self.array_image, bbs, utils.CLASS_DICT)
                     ros_img = self.bridge.cv2_to_imgmsg(image,  encoding="passthrough")
                     self.image_publisher.publish(ros_img)
