3d63fc6f245cce389de5d6a14814596fdd5e231b
rgce <rgce@kth.se>
2023-03-10 21:32:06 +0100

    :tada: working short- & long-term-memories for object recognition. (needs to be further tested)

--- src/bounding_box_detection/src/test.py
+++ src/bounding_box_detection/src/test.py
@@ -2,7 +2,7 @@ from collections import Counter
 from enum import Enum
 from dataclasses import dataclass
 import numpy as np
-from geometry_msgs.msg import PoseStamped
+#from geometry_msgs.msg import PoseStamped
 
 class Locations(Enum):
     MAP = "map"
@@ -10,13 +10,12 @@ class Locations(Enum):
     GRIPPER = "gripper"
     TRAY = "tray"
 
-@dataclass
-class DetectionObject:
-    class_name:str
-    position:PoseStamped #contains position in map frame and time of detection
+# @dataclass
+# class DetectionObject:
+#     class_name:str
+#     position:PoseStamped #contains position in map frame and time of detection
 
-
-class DetectionBuffer:
+class ShortTermMemory:
     """The detection buffer is a filter to reject random false detections.
     It keeps track of how many times an instance has been detected and how many times a class has been detected."""
 
@@ -24,27 +23,28 @@ class DetectionBuffer:
         self.instances_detected_counter = Counter() # keeps track of how many times the same instance has been detected
         self.class_counter = Counter() # keeps track of how many times a new element of every class has been detected
         self.last_time_seen = {}
-        self.average_positions= {}
+        self.average_position= {}
         self.distance_threshold = distance_threshold
         self.time_threshold = time_threshold
 
-    def _get_instance_name(self, class_name):
+    def get_next_instance_name(self, class_name):
         return class_name + "_" +  str(self.class_counter[class_name])
-    def _get_instance_position(self, instance_name):
-        return self.average_positions[instance_name]
+    def get_instance_position(self, instance_name):
+        return self.average_position[instance_name]
     
-    def _get_class_name(self, instance_name):
+    def get_class_name(self, instance_name: str):
+        print(instance_name.split("_")[0])
         return instance_name.split("_")[0]
 
-    def _check_position(self, position):
+    def check_position(self, position):
         keys = []
-        for key, value in self.average_positions.items():
+        for key, value in self.average_position.items():
             if np.linalg.norm(position - value) < self.distance_threshold:
                 keys.append(key)
-        keys = sorted(keys, key=lambda x: np.linalg.norm(position - self.average_positions[x]))
+        keys = sorted(keys, key=lambda x: np.linalg.norm(position - self.average_position[x]))
         return keys
     
-    def _check_time(self, timestamp):
+    def check_time(self, timestamp):
         """Deletes all instances that have not been seen for more than time_threshold"""
         keys = []
         for key, value in self.last_time_seen.items():
@@ -54,7 +54,7 @@ class DetectionBuffer:
         for key in keys:
             del self.instances_detected_counter[key]
             del self.last_time_seen[key]
-            del self.average_positions[key]
+            del self.average_position[key]
             # do we want to decrease the class counter as well? 
             # Problem: maybe we just deleted the last instance of a class but we want to add to a higher number.
             # What if everytime we called this we passed it the external counter?
@@ -64,29 +64,29 @@ class DetectionBuffer:
         """Increments the counter for the instance (whther it exists or not) and updates the average position"""
 
         if instance_name in self.instances_detected_counter:
-            self.average_positions[instance_name] = (self.instances_detected_counter[instance_name]/(self.instances_detected_counter[instance_name]+1))*(self.average_positions[instance_name] + position/(self.instances_detected_counter[instance_name]))
+            self.average_position[instance_name] = (self.instances_detected_counter[instance_name]/(self.instances_detected_counter[instance_name]+1))*(self.average_position[instance_name] + position/(self.instances_detected_counter[instance_name]))
             self.instances_detected_counter[instance_name] += 1 # we saw this instance again
             self.last_time_seen[instance_name] = timestamp
 
         else:
             self.instances_detected_counter[instance_name] = 1
-            self.average_positions[instance_name] = position
+            self.average_position[instance_name] = position
             self.class_counter[class_name] += 1 #we saw a new instance belonging to this class
             self.last_time_seen[instance_name] = timestamp
     
     def add(self, class_name, position, timestamp):
-        self._check_time(timestamp) # delete old instances (possible fix: only delete them if is was only seen for e.g. 3 frames)
-        keys = self._check_position(position)
+        self.check_time(timestamp) # delete old instances (possible fix: only delete them if is was only seen for e.g. 3 frames)
+        keys = self.check_position(position)
 
 
         for key in keys:
-            if len(keys) != 0 and self._get_class_name(key) == class_name:
+            if len(keys) != 0 and self.get_class_name(key) == class_name:
                 #print(f"adding {class_name} at {position} to {key}")
                 self._add(class_name, key, position, timestamp)
                 return
         
         #print(f"adding {class_name} at {position} to new instance")
-        instance_name = self._get_instance_name(class_name)
+        instance_name = self.get_next_instance_name(class_name)
         self._add(class_name, instance_name, position, timestamp)
 
 
@@ -108,6 +108,7 @@ class LongTermMemory:
     """Stores the objects that have been detected more than N times in the DetectionBuffer"""
 
     def __init__(self, frames_needed_for_reconition, distance_threshold) -> None:
+        self.class_counter = Counter() # keeps track of how many times a new element of every class has been detected
         self.instances_in_memory=[]
         self.locations={} # is the instance in the Map, Tray, Grip, Box?
         self.last_time_seen = {}
@@ -115,46 +116,52 @@ class LongTermMemory:
         self.distance_threshold = distance_threshold
         self.min_frames_needed = frames_needed_for_reconition # how many times the object has to be detected in the DetectionBuffer to be added to the LongTermMemory
 
-    def _get_instance_name(self, class_name):
+    def get_next_instance_name(self, class_name):
         return class_name + "_" +  str(self.class_counter[class_name])
     
-    def _get_class_name(self, instance_name):
+    def get_class_name(self, instance_name:str):
         return instance_name.split("_")[0]
 
-    def _check_position(self, position):
+    def check_position(self, position):
+        """Checks if there is an instance in the long term memory that is close enough
+          to the position we want to add and returns the name of the closest one"""
         keys = []
         for key, value in self.positions.items():
             if np.linalg.norm(position - value) < self.distance_threshold:
                 keys.append(key)
-        keys = sorted(keys, key=lambda x: np.linalg.norm(position - self.average_positions[x]))
-        return keys
+        keys = sorted(keys, key=lambda x: np.linalg.norm(position - self.positions[x]))
+        return keys[0]
             # do we want to decrease the class counter as well? Problem: maybe we just deleted the last instance of a class but we want to add to a higher number.
-            # What if everytime we called this we passed it the external counter?
 
     
-    def _add(self, class_name, instance_name, position, timestamp):
-        """Increments the elements in the Long term memory"""
-        
-        if class_name in [self._get_class_name(self, instance) for instance in self.instances_in_memory]: # PROBABLY AND  INCORRECT ITERATION?
+    def _updateMemory(self, class_name, instance_name, position, timestamp):
+        """Updates an old or creates a new element for the Long term memory"""
+        # If other instances of the same class exist in the long term memory check if they are close enough to the one we want to add.
+        # If they are, update their position and timestamp:
+        if class_name in [self.get_class_name(instance) for instance in self.instances_in_memory]: 
             #TODO set different locations (M,T,G,B) depending on where the obkect is
-            self.locations[instance_name] = "M" # we assume that the object is in the map (FOR NOW ONLY)
-            self.instances_in_memory.append(instance_name)
-            self.positions[instance_name] = position
-            self.last_time_seen[instance_name] = timestamp
-            return
-
-        instance_name = self._get_instance_name(class_name) # create a new instance name
+            closest_instance_in_lt_memory = self.check_position(db.average_position[instance_name])
+            if closest_instance_in_lt_memory != None:
+                self.locations[closest_instance_in_lt_memory] = "M" # !!!FOR NOW ONLY OBJECTS IN MAP ARE STORED IN THE LONG TERM MEMORY!!!
+                self.positions[closest_instance_in_lt_memory] = position
+                self.last_time_seen[closest_instance_in_lt_memory] = timestamp
+                return
+            
+        # if there is no instance in the long term memory of this class that is close enough to the one we want to add,
+        # create a new instance and add it to the long term memory
+        instance_name = self.get_next_instance_name(class_name) # create a new instance name
         self.instances_in_memory.append(instance_name)
         self.positions[instance_name] = position
         self.last_time_seen[instance_name] = timestamp
+        self.class_counter[class_name] += 1 #we saw a new instance belonging to this class
     
-    def add(self, timestamp, db: DetectionBuffer):
-        for key, value in db.instances_detected_counter.items():
-            if value > self.min_frames_needed:
-                keys = self._check_position(db.average_positions[key])
-                if len(keys) != 0:
-                    self._add(db._get_class_name(key), key, db._get_position(key), timestamp)
-                    return
+    def checkForObjectsToRemember(self, timestamp, db: ShortTermMemory):
+        """Checks if there are objects in the ShortTermMemory that have been detected more than N times and adds them to the LongTermMemory"""
+        for db_instance, counter in db.instances_detected_counter.items():
+            if counter > self.min_frames_needed:
+                self._updateMemory(self.get_class_name(db_instance), db_instance, db.get_instance_position(db_instance), timestamp)
+                
+            
 
 
 
@@ -164,10 +171,10 @@ class LongTermMemory:
         
 
 if __name__ == "__main__":
-    db = DetectionBuffer()
-    lt = LongTermMemory(3,.05)
-    print(db._get_class_name("person_0"))
-    print(db._get_instance_name("person"))
+    db = ShortTermMemory()
+    lt = LongTermMemory(1,.05)
+    print(db.get_class_name("person_0"))
+    print(db.get_next_instance_name("person"))
     db.add("person", np.array([0,0,0]),4)
     db.add("person", np.array([0.1,0.1,0.1]),4)
     print(db.instances_detected_counter)
@@ -181,5 +188,6 @@ if __name__ == "__main__":
     print(db.instances_detected_counter)
     db.add("kiki", -np.array([0.1,0.1,0.1]),4)
     print(db.instances_detected_counter)
-    lt.add(5,db)
+    lt.add(4,db)
+    print(lt.instances_in_memory)
     
