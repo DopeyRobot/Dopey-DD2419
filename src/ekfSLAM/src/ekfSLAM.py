617bec8bfbd1aab426776d80f9bc495dff8f4814
kurreman <korayamku@gmail.com>
2023-03-23 10:37:10 +0100

    :construction: fixed dimensionality of mu vector

--- src/ekfSLAM/src/ekfSLAM.py
+++ src/ekfSLAM/src/ekfSLAM.py
@@ -132,8 +132,8 @@ class EkfSLAM:
         self._inflate_matrices()
         # q = pose.pose.orientation
         # yaw = tf_conversions.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])[2]
-        self.mu_bar_t[:,-1] = np.array([[pose.pose.position.x],[pose.pose.position.y],[0]])
-        self.sigma_bar_t[-3:,-3:] = self.sigma_bar_t[3:,3:] #Inherit covariance from robot pose when seen
+        self.mu_bar_t[-3:,:] = np.array([[pose.pose.position.x],[pose.pose.position.y],[0]])
+        self.sigma_bar_t[-3:,-3:] = self.sigma_bar_t[:3,:3] #Inherit covariance from robot pose when seen
 
     def update_landmark(self,arucoID,pose):
         # q = pose.pose.orientation
@@ -141,7 +141,8 @@ class EkfSLAM:
         z = np.array([[pose.pose.position.x-self.mu_bar_t[0,0]],[pose.pose.position.y-self.mu_bar_t[1,0]],[0]])
         j = self.seenLandmarks.index(arucoID)
         N = self.seenLandmarks.shape[0]
-        z_hat = np.array([[self.mu_bar_t[0,j]-self.mu_bar_t[0,0]],[self.mu_bar_t[1,j]-self.mu_bar_t[1,0]],[0]])
+        # z_hat = np.array([[self.mu_bar_t[0,j]-self.mu_bar_t[0,0]],[self.mu_bar_t[1,j]-self.mu_bar_t[1,0]],[0]])
+        z_hat = np.array([[self.mu_bar_t[3+3*j,0]-self.mu_bar_t[0,0]],[self.mu_bar_t[4+3*j,0]-self.mu_bar_t[1,0]],[0]])
         H_prime = np.zeros((3,6))
         H_prime[0,0] = -1
         H_prime[0,3] = 1
@@ -158,25 +159,24 @@ class EkfSLAM:
         assert KH.shape[0] == KH.shape[1]
         self.sigma_bar_t = (np.eye(KH.shape[0]) - KH) @ self.sigma_bar_t
 
-
-
     def _inflate_matrices(self):
-        inf=1e9
-        #add 3 columns to F
-        self.Fx = np.hstack((self.Fx,np.zeros((3,3))))
-        #add 3 rows to F
-        #self.Fx = np.vstack((self.Fx,np.zeros((3,self.Fx.shape[1]))))
-        #set bottom right 3x3 to identity
-        self.Fx[-3:,-3:] = np.eye(3)
-        #add 1 column to mu and mu_bar
-        self.mu_t = np.hstack((self.mu_t,np.zeros((3,1))))
-        self.mu_bar_t = np.hstack((self.mu_bar_t,np.zeros((3,1))))
-        #add covariance matrix space for new landmark on sigma and sigma_bar
-        # self.sigma_t = np.hstack((self.sigma_t,np.eye(3)*inf))
-        # self.sigma_bar_t =  np.hstack((self.sigma_bar_t,np.eye(3)*inf))
-        #stack matrices on diagonal
-        self.sigma_t = block_diag(self.sigma_t,np.eye(3)*inf)
-        self.sigma_bar_t = block_diag(self.sigma_bar_t,np.eye(3)*inf)
+            inf=1e9
+            #add 3 columns to F
+            self.Fx = np.hstack((self.Fx,np.zeros((3,3))))
+            #add 3 rows to F
+            #self.Fx = np.vstack((self.Fx,np.zeros((3,self.Fx.shape[1]))))
+            #set bottom right 3x3 to identity
+            self.Fx[-3:,-3:] = np.eye(3)
+            #add 3 rows to mu and mu_bar
+            self.mu_t = np.vstack((self.mu_t,np.zeros((3,1))))
+            self.mu_bar_t = np.vstack((self.mu_bar_t,np.zeros((3,1))))
+            #add covariance matrix space for new landmark on sigma and sigma_bar
+            # self.sigma_t = np.hstack((self.sigma_t,np.eye(3)*inf))
+            # self.sigma_bar_t =  np.hstack((self.sigma_bar_t,np.eye(3)*inf))
+            #stack matrices on diagonal
+            self.sigma_t = block_diag(self.sigma_t,np.eye(3)*inf)
+            self.sigma_bar_t = block_diag(self.sigma_bar_t,np.eye(3)*inf)
+
     
     def _aruco_in_frame(self,pose,parent_frame):
         #if self.buffer.can_transform(parent_frame, self.aruco_frame, self.currHeaderStamp, rospy.Duration(2)):
