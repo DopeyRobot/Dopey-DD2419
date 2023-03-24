e11b23c2025474c3fd78aab5487ba60239cdbca8
DopeyRobot <dopeyrobot5000@gmail.com>
2023-03-24 17:27:10 +0100

    fkjsdlf

--- src/occupancy_grid/point_cloud_to_laserscan.py
+++ src/occupancy_grid/point_cloud_to_laserscan.py
@@ -15,21 +15,22 @@ class PointCloudToLaserScan:
         )
         self.pub_laserscan = rospy.Publisher("/laserscan", LaserScan, queue_size=10)
 
-        self.voxel_size = 0.05
+        self.voxel_size = 0.01
         ## laserscan parameters
         self.min_angle = -1.57
         self.max_angle = 1.57
         self.angle_increment = 0.01
         self.scan_time = 1 / self.f
         self.range_min = 0.1
-        self.range_max = 1.5
+        self.range_max = 3.0
 
         self.target_frame = "map"
-        self.source_frame = "camera_color_optical_frame"
+        self.source_frame = "camera_color_frame"
 
     def cloud_callback(self, msg):
         laserscan = LaserScan()
         laserscan.header = msg.header
+        laserscan.header.frame_id = self.source_frame
 
         laserscan.angle_min = self.min_angle
         laserscan.angle_max = self.max_angle
@@ -39,7 +40,7 @@ class PointCloudToLaserScan:
         laserscan.range_max = self.range_max
 
         N_ranges = int((self.max_angle - self.min_angle) / self.angle_increment)
-        ranges = np.zeros(N_ranges)
+        ranges = np.ones(N_ranges)*self.range_max
 
         # transform pointcloud to map frame
 
@@ -52,35 +53,35 @@ class PointCloudToLaserScan:
         # Convert Open3D -> NumPy
         points = np.asarray(cloud.points)
         height_req = points[:, 1] < 0.10
-        isnanx = np.isnan(points[:, 0])
-        isnany = np.isnan(points[:, 1])
-        isnanz = np.isnan(points[:, 2])
-        isnan = np.logical_or(isnanx, isnany)
-        isnan = np.logical_or(isnan, isnanz)
-        final_mask = np.logical_and(height_req, np.logical_not(isnan))
+        # isnanx = np.isnan(points[:, 0])
+        # isnany = np.isnan(points[:, 1])
+        # isnanz = np.isnan(points[:, 2])
+        # isnan = np.logical_or(isnanx, isnany)
+        # isnan = np.logical_or(isnan, isnanz)
+        # final_mask = np.logical_and(height_req, np.logical_not(isnan))
 
-        dist_aboveground = points[final_mask]  # these will be occupied spaces
+        dist_aboveground = points[height_req]  # these will be occupied spaces
 
         for point in dist_aboveground:
             # get angle of point
-            x = point[0]
-            y = point[2]
+            x = point[2]
+            y = -point[0]
             angle = np.arctan2(y, x)
             # get range of point
-            range = np.hypot(x, y)
-
+            distance = np.hypot(x, y)
+            # print(distance)
             if angle < self.min_angle or angle > self.max_angle:
                 continue
-            if range < self.range_min or range > self.range_max:
+            if distance < self.range_min or distance > self.range_max:
                 continue
 
-            # get index of range
+            # get index of distance
             index = int((angle - self.min_angle) / self.angle_increment)
 
-            # check if range is smaller than current range
-            if range < ranges[index]:
-                ranges[index] = range
-
+            # check if distance is smaller than current distance
+            if distance < ranges[index]:
+                ranges[index] = distance
+                
         laserscan.ranges = ranges
         self.pub_laserscan.publish(laserscan)
 
